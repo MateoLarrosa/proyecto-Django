bc8d3133f44f1ffc884fe43d723ed53eca55285d
Mateo C. Larrosa <mateoclarrosa@gmail.com>
2023-03-30 23:16:03 -0300

    Cambios respectivos a la clase 20, playgroundintermedioI

--- app1/admin.py
+++ app1/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from app1.models import Animal,Familiares
 # Register your models here.
+
+admin.site.register([Animal,Familiares])
--- app1/urls.py
+++ app1/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 from app1 import views
 
+app_name = 'app1'
+
 urlpatterns = [
-    path('index/',views.index),
-    path('mostrar-fecha/',views.mostrar_fecha),
+    path('',views.index,name='index'),
+    path('mostrar-fecha/',views.mostrar_fecha,name='mostrar_fecha'),
     #pasando parametros por URL
-    path('suma/<int:num1>/<int:num2>/',views.suma),
-    path('otro-template/',views.prueba_template),
-    path('crear-animal/',views.crear_animal),
-    path('prueba-render/',views.prueba_render),
-    path('familia-mateo/',views.mi_familia),
+    path('suma/<int:num1>/<int:num2>/',views.suma,name='suma'),
+    path('otro-template/',views.prueba_template,name='otro_template'),
+    path('crear-animal/',views.crear_animal,name='crear_animal'),
+    path('prueba-render/',views.prueba_render,name='prueba_render'),
+    path('familia-mateo/',views.mi_familia,name='familia_mateo'),
     #path('mi-primer-template/',views.mi_primer_template),
 ]
